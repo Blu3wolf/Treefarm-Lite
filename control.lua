d068b2f2573a097ee280c869b47a11e9c37f5cb4
Blue <blue@blu3wolf.com>
2016-01-16 09:36:48 +0800

    Update control.lua

--- control.lua
+++ control.lua
@@ -47,7 +47,7 @@ end)
 
 
 
-script.on_load(function()
+script.on_configuration_changed(function()
   for _, plantTypes in pairs(global.tf.seedPrototypes) do
     if plantTypes.efficiency.other == 0 then
       plantTypes.efficiency.other = 0.01
