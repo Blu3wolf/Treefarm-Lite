890200d8bf22223bc2aed9d224c7597161eff42b
Blue <blue@blu3wolf.com>
2016-03-01 20:17:16 +0800

    Update control.lua

--- control.lua
+++ control.lua
@@ -45,28 +45,28 @@ script.on_event(defines.events.on_player_created, function(event)
   end
 end)
 
+script.on_event(defines.events.on_tick, function(event)
+	local loaded = false
+	if loaded ~= true then
+		for _, plantTypes in pairs(global.tf.seedPrototypes) do
+			if plantTypes.efficiency.other == 0 then
+				plantTypes.efficiency.other = 0.01
+			end
+		end
 
+		for seedTypeName, seedPrototype in pairs (global.tf.seedPrototypes) do
+			if game.item_prototypes[seedPrototype.states[1]] == nil then
+				global.tf.seedPrototypes[seedTypeName] = nil
+			end
+		end
 
-script.on_load(function()
-  for _, plantTypes in pairs(global.tf.seedPrototypes) do
-    if plantTypes.efficiency.other == 0 then
-      plantTypes.efficiency.other = 0.01
-    end
-  end
-
-  for seedTypeName, seedPrototype in pairs (global.tf.seedPrototypes) do
-    if game.item_prototypes[seedPrototype.states[1]] == nil then
-      global.tf.seedPrototypes[seedTypeName] = nil
-    end
-  end
-
-  if seedTypeLookUpTable ~= nil then
-    seedTypeLookUpTable = {}
-  end
-  populateSeedTypeLookUpTable()
+		if seedTypeLookUpTable ~= nil then
+			seedTypeLookUpTable = {}
+		end
+		populateSeedTypeLookUpTable()
+		loaded = true
+	end
 end)
-
-
 
 script.on_event(defines.events.on_gui_click, function(event)
   local index = -1
